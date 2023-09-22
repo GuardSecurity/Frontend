8cf437a1317623a0de1b94457de082fb9bde5ee3
cmvn-nghianguyen <nghia.nguyen@classmethod.vn>
2023-09-23 02:29:37 +0700

    Adding the navbar

---
+++ frontend/src/component/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Logo = () => {
+  return (
+    <div className='flex gap-1'>
+      <img src={require('../assets/img/Group.png')} />
+      <h1>Guard</h1>
+    </div>
+  );
+}
+
+export default Logo
