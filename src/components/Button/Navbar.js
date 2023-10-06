d45c1dd310c6cdcd85736f475964a83d1b2a199e
7evenice <vermas8900@gmail.com>
2023-10-06 19:23:52 +0700

    Adding the component navbar

---
+++ src/components/Button/Navbar.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import logo from '../../assets/logo.png';
+import BaseButton from './Button';
+
+const Navbar = () => {
+  const navigation = [
+    { name: 'About', href: '#', current: true },
+    { name: 'Booking', href: '#', current: false },
+    { name: 'Contact', href: '#', current: false },
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
+  return (
+    <>
+      <Disclosure as='nav' className='bg-white'>
+        {({ open }) => (
+          <>
+            <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+              <div className='flex h-16 items-center justify-between'>
+                <div className='flex items-center'>
+                  <div className='flex flex-row items-center justify-between'>
+                    <img className='w-7 h-7' src={logo} alt='Your Company' />
+                    <div className='w-24 ml-5 text-gray-400 text-3xl font-bold'>Guard</div>
+                  </div>
+                  <div className='hidden md:block'>
+                    <div className='ml-12 flex items-baseline space-x-11'>
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current ? 'text-[#C7923E]' : 'text-neutral-600',
+                            'font-medium w-[66.75px]'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className='hidden md:block'>
+                  <div className='ml-4 flex items-center md:ml-6'>
+                    <BaseButton content={'Login'} />
+                  </div>
+                </div>
+                <div className='-mr-2 flex md:hidden'>
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className='relative inline-flex items-center justify-center rounded-md text-[#C7923E] p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                    <span className='absolute -inset-0.5' />
+                    <span className='sr-only'>Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
+                    ) : (
+                      <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
+                    )}
+                  </Disclosure.Button>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className='md:hidden'>
+              <div className='space-y-1 px-2 pb-3 pt-2 sm:px-3'>
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as='a'
+                    href={item.href}
+                    className={classNames(
+                      item.current ? 'text-[#C7923E]' : 'text-neutral-600',
+                      'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+              <div className='border-t border-gray-700 pb-3 pt-4'>
+                <div className='flex items-center px-5'>
+                  <BaseButton content={'Login'} />
+                </div>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </>
+  );
+};
+
+export default Navbar;
