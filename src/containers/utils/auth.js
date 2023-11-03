710f3a87f03f9df419762208800512b9d761ae80
7evenice <vermas8900@gmail.com>
2023-11-03 19:37:49 +0700

    Config Auth SignUp & Login

---
+++ src/containers/utils/auth.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
+
+const configuration = (method, path, data) => ({
+  method: method,
+  url: `${BASE_URL}${path}`,
+  data: data,
+});
+
+const signUp = ({
+  username,
+  passwd,
+  confirmpasswd,
+  role,
+  firstname,
+  lastname,
+  phone,
+}) => {
+  return axios(
+    configuration("post", "/auth/signup", {
+      username,
+      passwd,
+      confirmpasswd,
+      role,
+      firstname,
+      lastname,
+      phone,
+    })
+  )
+    .then((result) => result)
+    .catch((error) => error);
+};
+
+const login = (username, passwd) => {
+  return axios(configuration("post", "/auth/login", { username, passwd }))
+    .then((result) => result.data)
+    .catch((error) => error);
+};
+
+export { signUp, login };
